d9276c521811a169d044f9c967b358a0508e6a9c
David Calcus <targoo@gmail.com>
2015-05-28 08:14:43 +0100

    start theming

---
+++ www/app/speciality/specialityLogo.directive.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('pcb.speciality')
+        .directive('specialityLogo', specialityLogo);
+
+    specialityLogo.$inject = [];
+
+    function specialityLogo() {
+
+        return {
+            templateUrl: 'app/speciality/templates/logo.html',
+            restrict: 'E',
+            scope: {
+                label: "@label",
+                icon: "@icon"
+            }
+            //scope: {
+            //    sessions: '=',
+            //    filterObject: '=?',
+            //    showTimeSlots: '=?'
+            //},
+            //controller: function($scope) {
+            //    $scope.filterObject = $scope.filterObject || undefined;
+            //    $scope.showTimeSlots = $scope.showTimeSlots || false;
+            //}
+        };
+        ////////////////////////////////////////////////////////////////////////
+
+    }
+
+})();
